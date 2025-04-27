f5ae80c4f409c68487c8f051a08ac1015ebedd30
Bassam-Elsaied <bassame993@gmail.com>
2025-04-27 14:57:31 +0300

    fix resume

--- app/[locale]/resume/page.jsx
+++ app/[locale]/resume/page.jsx
@@ -3,6 +3,11 @@ import { Container } from "@/components/Container";
 import { Heading } from "@/components/Heading";
 import { WorkHistory } from "@/components/WorkHistory";
 
+import { Container } from "@/components/Container";
+
+import { Heading } from "@/components/Heading";
+import { WorkHistory } from "@/components/WorkHistory";
+
 import { useLocale, useTranslations } from "next-intl";
 
 export default function Home() {
@@ -17,16 +22,11 @@ export default function Home() {
         <span className="bg-neutral-100 px-1 py-0.5">{t("spanOne")}</span>
         {t("descriptionTwo")}
       </p>
-      {/* <p className="max-w-xl mt-4 text-sm lg:text-base font-normal text-secondary">
-        {t("descriptionThree")}
-        <span className="bg-neutral-100 px-1 py-0.5">{t("spanTwo")}</span>
-        {t("descriptionFour")}
-      </p> */}
 
       <div className="flex justify-center items-center">
         <button className="mt-6  relative inline-block cursor-pointer no-underline group rounded-full bg-slate-900 p-2 text-xs font-semibold leading-6 text-white shadow-2xl ">
-          <a href="/Bassam Elsayed - Front-end .pdf" download>
-            {locale === "en" ? "Download cv" : " تحميل السيرة الذاتيه"}
+          <a href="/Bassam Elsayed - Front-end.pdf" download>
+            {locale === "en" ? "Download cv" : "تحميل السيرة الذاتيه"}
           </a>
         </button>
       </div>
